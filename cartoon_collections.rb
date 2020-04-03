73d69d8be9bc02be5ceadc36de9eacf36bdae6fc
chris brady <chrisb3546@yahoo.com>
2020-04-03 14:36:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,61 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+ dwarves = %w[Dopey Grumpy Bashful]
+
+def roll_call_dwarves(dwarves)
+  
+  dwarves.each.with_index(1) do |index, dwarf|
+    puts "#{dwarf}. #{index}"
+  end
+end 
+
+roll_call_dwarves(dwarves)
+
+fruits = %w[apple banana orange]
+
+def summon_captain_planet(fruits)
+  fruitsb = []
+  fruits.each do |call|
+     fruitsb << "#{call.capitalize}!"
+end
+fruitsb
 end
 
-def summon_captain_planet# code an argument here
+summon_captain_planet(fruits)
+
+calls_long = ["axe", "earth", "wind", "fire"]
+
+
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |word| 
+    word.length > 4# code an argument here
   # Your code here
 end
+end
+long_planeteer_calls(calls_long)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+no_cheese = ["ham", "cellphone", "computer"]
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese(maybe_cheese)
+  
+    cheese_types= ["cheddar", "gouda", "camembert"]
+ maybe_cheese.find do |cheese|
+   cheese_types.include? cheese 
+   
+   
+     
+ 
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+
+
+
+
+
+
+
